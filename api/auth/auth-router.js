644316e8fff0be1d38b37e6b1e49528751737d0b
Jennifer Kramer <jenkrame@gmail.com>
2021-02-23 20:45:58 -0500

    Created secret in config file and linked it. Created post requests for register and login.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,86 @@
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../../config/secrets.js");
 
+const router = require("express").Router();
 
-//| POST   | /api/register | Creates a `user` using the information sent inside the `body` of the request. **Hash the password** before saving the user to the database.                                                                                                                            |
+const Users = require("../users/users-model.js");
+const { isValid } = require("../users/users-service.js");
+
+//| POST   | /api/register | Creates a `user` using the information sent inside the `body` of the request. **Hash the password** before saving the user to the database.
+
+router.post("/register", (req, res) => {
+    const credentials = req.body;
+
+    if(isValid(credentials)) {
+        // eslint-disable-next-line no-undef
+        const rounds = process.env.BCRYPT_ROUND || 8;
+        credentials.password = hash;
+
+        const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+        Users.add(credentials)
+            .then((user) => {
+                res.status(201).json({
+                    data: user
+                });
+            })
+            .catch((error) => {
+                res.status(500).json({
+                    message: error.message
+                });
+            });
+    } else {
+        res.status(400).json({
+            message: "Provide username and password. Password should be alphanumeric."
+        });
+    }
+});
+
 // | POST   | /api/login    | Use the credentials sent inside the `body` to authenticate the user. On successful login, create a new JWT with the user id as the subject and send it back to the client. If login fails, respond with the correct status code and the message: 'You shall not pass!'
 
-// | GET    | /api/users    | If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in respond with the correct status code and the message: 'You shall not pass!'.
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if (isValid(req.body)) {
+        Users.findBy({ username: username })
+            .then(([user]) => {
+                if (user && bcryptjs.compareSync(password, user.password)) {
+                    const token = makeToken(user);
+
+                    res.status(200).json({
+                        message: "Welcome back, " + user.username,
+                        token
+                    });
+                } else {
+                    res.status(401).json({
+                        message: "Invalid credentials!"
+                    });
+                }
+            })
+            .catch((error) => {
+                res.status(500).json({
+                    message: error.message
+                });
+            });
+    } else {
+        res.status(400).json({
+            message: "Please provide username and password. Password should be alphanumeric."
+        });
+    }
+});
+
+function makeToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        dept: user.dept
+    };
+    const options = {
+        expiresIn: "500s"
+    };
+
+    return jwt.sign(payload, jwtSecret, options);
+}
+
+module.exports = router;
--- config/secrets.js
+++ config/secrets.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 const jwtSecret = process.env.JWT_SECRET || "secrets";
 
 module.exports = {
